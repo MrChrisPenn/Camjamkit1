321e7452372467963509e98eb5686b7aaa6d4690
ncscomputing <cnpenn@gmail.com>
2018-04-03 21:48:11 +0000

    Add files via upload

---
+++ 4 TurningOnCamJamTrafficLights_Switch.py
@@ -0,0 +1,50 @@
+#hacked together version to be triggered from Minecraft by @ChrisPenn84
+
+#import minecraft libraries
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+
+# CamJam EduKit 1 - Basics
+# Worksheet 2 - LEDs
+
+# Import Libraries
+import time  # A collection of time related commands
+from gpiozero import LED  # The LED functions from GPIO Zero
+from gpiozero import Button
+
+# Set pins 18, 23 and 24 to be LEDs
+red = LED(18)
+yellow = LED(23)
+green = LED(24)
+Switch = Button(3)
+
+mc = minecraft.Minecraft.create()
+woolId = 46
+
+def Light_Sequence():
+    mc.postToChat("Cam Jam Edukit 1 Minecraft / Breadboard Traffic Lights :)")
+    time.sleep(1)
+    mc.postToChat("red on")
+    red.on()
+    time.sleep(4)
+    red.off()
+    mc.postToChat("red off")
+    time.sleep(0.5)
+    mc.postToChat("yellow on")
+    yellow.on()
+    time.sleep(4)
+    mc.postToChat("yellow off")
+    yellow.off()
+    time.sleep(0.5)
+    mc.postToChat("Green on")
+    green.on()
+    time.sleep(4)
+    mc.postToChat("Green off")
+    green.off()
+    time.sleep(1)
+    mc.postToChat("LEDs off")
+
+while True:
+    Switch.wait_for_press()
+    Light_Sequence()
+    time.sleep(2)
